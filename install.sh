c14a31cb5b2f541c1851859452533ed9054237a9
Bbanez <banezprod@gmail.com>
2022-02-09 17:09:18 +0100

    Fix unsafe

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 sudo apt update
 sudo apt install nodejs npm -y
-sudo npm i -g n
+sudo npm i -g n --unsafe-perm
 sudo n 14
-sudo npm i -g ./bcms-cli.tgz
+sudo npm i -g ./cli.tgz --unsafe-perm
 
 echo "bcms --instance 6169756ef956f26df700c2d7 --install --terminal-login" > install
 chmod 755 install
